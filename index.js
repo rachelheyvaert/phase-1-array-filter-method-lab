8cfb106d822ccc74d2c97e1c6217367f859d3569
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-18 07:47:14 -0500

    commit tree 08c48f45fc4e6baab682c0279538f6862e235023

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const { stringContaining } = require("expect");
+
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(drivers, string) {
+    const answer = drivers.filter(driversName => driversName.startsWith(string))
+    return answer;
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(hometown){
+      return hometown.name === name
+    })
+  }
